5c597810d8c30c5929ff61a6882e990e2c2deb3b
captainhook <brabdullahfahim@gmail.com>
2019-10-12 13:43:42 +0100

    Update IPLookup.ps1

--- IPLookup.ps1
+++ IPLookup.ps1
@@ -6,22 +6,22 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-# get IP
-$Dyn_IP = Resolve-DnsName -Name domain.com -Type A -DnsOnly | Select IPAddress -ExpandProperty IPAddress
+# get IP only, strip all other columns, strip header
+$Dyn_IP_siteA = Resolve-DnsName -Name siteA.domain.com -Type A -DnsOnly | Select IPAddress -ExpandProperty IPAddress
 
-# Interact with query parameters or the body of the request.
+# Require a query parameter/value as a layer of abstraction/variability. This is not necessary but a) ensures anyone with your function URL has to know a parameter and its possible values, b) allows you to expand the function for multiple values.
 $name = $Request.Query.Name
 if (-not $name) {
     $name = $Request.Body.Name
 }
 
-if ($name -eq "someName") {
+if ($name -eq "Office1") {
     $status = [HttpStatusCode]::OK
-    $body = $Dyn_IP
+    $body = $Dyn_IP_siteA
 }
 else {
     $status = [HttpStatusCode]::BadRequest
-    $body = "Bad Request."
+    $body = "Bad Request Logged"
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
